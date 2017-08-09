71e862d962fd811714ef7d37c33b13ada70c3979
bkap1192 <harish.selenium31@gmail.com>
2017-08-09 10:44:25 +0530

    9 august 2nd commit

--- src/test/java/com/hotelogix/languageSmoke/test/Admin/AdminTest_PT.java
+++ src/test/java/com/hotelogix/languageSmoke/test/Admin/AdminTest_PT.java
@@ -11,8 +11,11 @@ import org.testng.annotations.Test;
 import com.hotelogix.languageSmoke.BaseUtils.BasePage;
 import com.hotelogix.languageSmoke.LoginPage.LoginClass;
 import com.hotelogix.languageSmoke.RoomManager.Amenities.AddAmenity;
-import com.hotelogix.languageSmoke.RoomManager.Amenities.AmenitiesLandingPage;
+import com.hotelogix.languageSmoke.RoomManager.Amenities.AmenitiesListLandingPage;
+import com.hotelogix.languageSmoke.RoomManager.RoomTypes.EditRoomType;
 import com.hotelogix.languageSmoke.RoomManager.RoomTypes.RoomTypesLandingPage;
+import com.hotelogix.languageSmoke.RoomManager.Rooms.AddARoomPage;
+import com.hotelogix.languageSmoke.RoomManager.Rooms.RoomsListLandingPage;
 import com.hotelogix.languageSmoke.admin.GenericClass.ExcelUtils;
 import com.hotelogix.languageSmoke.admin.GenericClass.GenericMethods;
 import com.hotelogix.languageSmoke.admin.GenericClass.VerifyUtils;
@@ -54,13 +57,17 @@ public class AdminTest_PT {
 	    }
 	
 	
-	@Test(priority=2,description="Added amenity gets displayed on 'Amenities List' page and is also displayed on 'Add a Room Type' page under 'Select Amenities' >> 'Available Amenities'")
+
+	//@Test(priority=2,description="Added amenity gets displayed on 'Amenities List' page and is also displayed on 'Add a Room Type' page under 'Select Amenities' >> 'Available Amenities'")
 	public void fn_verifyAdditionOfAmenity() throws Exception{
 		try{
+		
 			String methodname=Thread.currentThread().getStackTrace()[1].getMethodName();
+//			HMM=ExcelUtils.UI().getTestCaseDataMap(Path, sheetname);
+//			HM=HMM.get(methodname);
 			HM=ExcelUtils.UI().getTestCaseDataMap(Path, sheetname, methodname);
 			BasePage.AHP().fn_NavigateAnyModule(GenericMethods.GI().getWebElement("A_AdminHomePage_RoomsManager"), GenericMethods.GI().getWebElement("A_AdminHomePage_Amenities"));			
-		    AmenitiesLandingPage ALP=new AmenitiesLandingPage();
+		    AmenitiesListLandingPage ALP=new AmenitiesListLandingPage();
 			VerifyUtils.VU().fn_AsserEquals(GenericMethods.GI().driver.getTitle(), HM.get("AmentitiesList_Title"));
 			ALP.fn_clkAddAmenity();
 			AddAmenity AA=new AddAmenity();
@@ -73,15 +80,35 @@ public class AdminTest_PT {
 		    VerifyUtils.VU().fn_AssertContainsInArray(l1, AA.amenityName);
 		    BasePage.AHP().fn_NavigateAnyModule(GenericMethods.GI().getWebElement("A_AdminHomePage_RoomsManager"),GenericMethods.GI().getWebElement("A_AdminHomePage_RoomTypes"));
 		    RoomTypesLandingPage RTLP=new RoomTypesLandingPage();
-		    
-		    
-		    
-		    
-		    
+		    RTLP.fn_clkEdit();
+		    EditRoomType ERT=new EditRoomType();
+		    ArrayList<String> arr1=ERT.fn_getAmenities();
+		    VerifyUtils.VU().fn_AssertContainsInArray(arr1, AA.amenityName);		    		    
 		}catch(Exception e){
 			throw e;
 		}
 	    }
+	
+	
+	@Test(priority=3,description="Added Room is displayed on 'Rooms List' page with status as active(green tick)")
+	public void fn_verifyAdditionOfRoom() throws Exception{
+		String methodname=Thread.currentThread().getStackTrace()[1].getMethodName();
+		HM=ExcelUtils.UI().getTestCaseDataMap(Path, sheetname, methodname);
+        BasePage.AHP().fn_NavigateAnyModule(GenericMethods.GI().getWebElement("A_AdminHomePage_RoomsManager"), GenericMethods.GI().getWebElement("A_AdminHomePage_Rooms"));
+        RoomsListLandingPage RLP=new RoomsListLandingPage();
+        VerifyUtils.VU().fn_AsserEquals(GenericMethods.GI().driver.getTitle(), HM.get("RoomsList_Title"));
+        RLP.fn_clkAddARoom();
+        AddARoomPage ARP=new AddARoomPage();
+        ARP.fn_fillAddRoomDetails();
+        String text=RLP.fn_verifyMsg();
+        VerifyUtils.VU().fn_AsserEquals(text,HM.get("Message_Text1")+'"'+ARP.roomName+'"'+HM.get("Message_Text2") );
+        
+        
+        
+        
+        
+        
+	}
 	
 	@AfterMethod
 	public void fn_close(){
